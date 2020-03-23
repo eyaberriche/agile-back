a26b5e68f924ade75dbf1dd8cee3886198e274ce
Eya Berriche <eyaberriche2@gmail.com>
2020-03-23 11:57:46 +0100

    test

--- src/main/java/org/isamm/Agile/AgileApplication.java
+++ src/main/java/org/isamm/Agile/AgileApplication.java
@@ -13,7 +13,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
-import org.springframework.context.ApplicationContext;
+
 
 @SpringBootApplication
 @EnableAutoConfiguration(exclude = {SecurityAutoConfiguration.class})
