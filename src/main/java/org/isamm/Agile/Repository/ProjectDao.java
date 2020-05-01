3a07aa0969c69ef4705c7b4e567cf4857895ef88
Eya Berriche <eyaberriche2@gmail.com>
2020-05-02 00:45:21 +0200

    correction du bd & code + methodes +list of actors

--- src/main/java/org/isamm/Agile/Repository/ProjectDao.java
+++ src/main/java/org/isamm/Agile/Repository/ProjectDao.java
@@ -6,9 +6,6 @@ import java.util.List;
 
 import org.isamm.Agile.model.Project;
 import org.springframework.data.jpa.repository.*;
-import org.springframework.data.repository.query.Param;
-import org.springframework.data.rest.core.annotation.RepositoryRestResource;
-import org.springframework.data.rest.core.annotation.RestResource;
 import org.springframework.stereotype.Repository;
 
 @Repository
