1708217ecff48033532329c80c2610968a29bc77
zayneb jelassi <zayneb182@gmail.com>
2020-07-01 00:31:05 +0200

    k

--- src/main/java/org/isamm/Agile/web/UserStoryController.java
+++ src/main/java/org/isamm/Agile/web/UserStoryController.java
@@ -94,6 +94,7 @@ private SprintDao sprintdao ;
              throws ResourceNotFoundException {
           return userStorydao.findByBacklog(id);}
 
+
           @GetMapping("allbySprint/{id}")
           public List<UserStory> getUsLibereBysprint(@PathVariable(value = "id") Long id)
              throws ResourceNotFoundException {
